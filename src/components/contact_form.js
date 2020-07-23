11277b4f375b5f14b18868d30661d8148f91df1d
Jean Antunes <jeanrantunes@MacBook-Pro-de-Jean.local>
2020-07-23 16:28:49 -0300

    test form integration

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -1,76 +1,84 @@
 import React from "react";
 import SbEditable from "storyblok-react";
-import axios from "axios";
-import * as qs from "query-string";
+import { navigate } from "gatsby-link";
 
-class ContactFormPage extends React.Component {
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
+
+export default class Contact extends React.Component {
   constructor(props) {
     super(props);
-    this.domRef = React.createRef();
-    this.state = { feedbackMsg: null };
+    this.state = {};
   }
 
-  handleSubmit(event) {
-    // Do not submit form via HTTP, since we're doing that via XHR request.
-    event.preventDefault();
-    // Loop through this component's refs (the fields) and add them to the
-    // formData object. What we're left with is an object of key-value pairs
-    // that represent the form data we want to send to Netlify.
-    const formData = {};
-    Object.keys(this.refs).map((key) => (formData[key] = this.refs[key].value));
-    // Set options for axios. The URL we're submitting to
-    // (this.props.location.pathname) is the current page.
-    const axiosOptions = {
-      url: "/",
-      method: "post",
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      data: qs.stringify(formData),
-    };
-
-    // Submit to Netlify. Upon success, set the feedback message and clear all
-    // the fields within the form. Upon failure, keep the fields as they are,
-    // but set the feedback message to show the error state.
-    axios(axiosOptions)
-      .then((response) => {
-        this.setState({
-          feedbackMsg: "Form submitted successfully!",
-        });
-        this.domRef.current.reset();
-      })
-      .catch((err) =>
-        this.setState({
-          feedbackMsg: "Form could not be submitted.",
-        })
-      );
-  }
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...this.state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch((error) => alert(error));
+  };
 
   render() {
     return (
       <SbEditable content={this.props.blok}>
         <h1>Contact</h1>
-
-        {this.state.feedbackMsg && <p>{this.state.feedbackMsg}</p>}
-
         <form
-          ref={this.domRef}
-          name="contact-form"
-          method="POST"
+          name="contact"
+          method="post"
+          action="/success/"
           data-netlify="true"
-          onSubmit={(event) => this.handleSubmit(event)}
+          data-netlify-honeypot="bot-field"
+          onSubmit={this.handleSubmit}
         >
-          <input
-            ref="form-name"
-            type="hidden"
-            name="form-name"
-            value="contact-form"
-          />
-          <input ref="email" type="email" name="email" />
-          <textarea ref="message" name="message" />
-          <button type="submit">Submit</button>
+          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p hidden>
+            <label>
+              Don’t fill this out:{" "}
+              <input name="bot-field" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your name:
+              <br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:
+              <br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:
+              <br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
         </form>
       </SbEditable>
     );
   }
 }
-
-export default ContactFormPage;
