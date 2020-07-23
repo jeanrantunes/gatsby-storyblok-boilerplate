9d03495091db330ac96408145401b0f66684584b
Jean Antunes <jeanrantunes@MacBook-Pro-de-Jean.local>
2020-07-23 12:43:57 -0300

    test form integration

--- src/components/components.js
+++ src/components/components.js
@@ -1,23 +1,25 @@
-import Page from './page'
-import Grid from './grid'
-import Teaser from './teaser'
-import Feature from './feature'
-import NavItem from './nav_item'
-import ComponentNotFound from './component_not_found'
+import Page from "./page";
+import Grid from "./grid";
+import Teaser from "./teaser";
+import Feature from "./feature";
+import NavItem from "./nav_item";
+import Form from "./contact_form";
+import ComponentNotFound from "./component_not_found";
 
 const ComponentList = {
   page: Page,
   grid: Grid,
   teaser: Teaser,
   feature: Feature,
-  nav_item: NavItem
-}
+  nav_item: NavItem,
+  contact_form: Form,
+};
 
 const Components = (type) => {
-  if (typeof ComponentList[type] === 'undefined') {
-    return ComponentNotFound
+  if (typeof ComponentList[type] === "undefined") {
+    return ComponentNotFound;
   }
-  return ComponentList[type]
-}
+  return ComponentList[type];
+};
 
-export default Components
+export default Components;
