61acce1388095bf515bd4384cd37d4448b7a4a82
Jean Antunes <jeanrantunes@MacBook-Pro-de-Jean.local>
2020-07-23 14:32:20 -0300

    test form integration

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -5,8 +5,13 @@ const ContactForm = (props) => (
   <SbEditable content={props.blok}>
     <h1>Contact</h1>
 
-    <form name="Contact Form" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="Contact Form" />
+    <form
+      name="contact"
+      method="POST"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+    >
+      <input type="hidden" name="contact" value="contact" />
       <div>
         <label>Your Email:</label>
         <input type="email" name="email" />
