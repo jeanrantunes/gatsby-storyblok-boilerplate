86e171bc44a86ada85e1532d0e846151b944f7b1
Jean Antunes <jeanrantunes@MacBook-Pro-de-Jean.local>
2020-07-23 15:10:44 -0300

    test form integration

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -4,16 +4,6 @@ import SbEditable from "storyblok-react";
 const ContactForm = (props) => (
   <SbEditable content={props.blok}>
     <h1>Contact</h1>
-
-    <form
-      name="contact"
-      method="post"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input name="name" placeholder="Your name" type="text" />
-      <button type="submit">submit</button>
-    </form>
   </SbEditable>
 );
 
