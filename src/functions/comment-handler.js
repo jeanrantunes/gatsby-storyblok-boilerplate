de2ece0051211a4ea7180d84157c34846cdb3272
Jean Antunes <jeanrantunes@MacBook-Pro-de-Jean.local>
2020-07-24 12:28:09 -0300

    test

--- src/functions/comment-handler.js
+++ src/functions/comment-handler.js
@@ -12,62 +12,59 @@ const URL = "https://elegant-wiles-a8b2e1.netlify.app";
 */
 exports.handler = async (event, context, callback) => {
   // get the arguments from the notification
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World",
-  });
+  console.log(event.body);
   // const body = JSON.parse(event.body);
-  // // // prepare call to the Slack API
-  // const slackURL = process.env.SLACK_WEBHOOK_URL;
-  // const slackPayload = {
-  //   text: "New comment on " + URL,
-  //   attachments: [
-  //     {
-  //       fallback: "New comment on the comment example site",
-  //       color: "#444",
-  //       author_name: body.data.email,
-  //       title: body.data.path,
-  //       title_link: URL + body.data.path,
-  //       text: body.data.comment,
-  //     },
-  //     {
-  //       fallback: "Manage comments on " + URL,
-  //       callback_id: "comment-action",
-  //       actions: [
-  //         {
-  //           type: "button",
-  //           text: "Approve comment",
-  //           name: "approve",
-  //           value: body.id,
-  //         },
-  //         {
-  //           type: "button",
-  //           style: "danger",
-  //           text: "Delete comment",
-  //           name: "delete",
-  //           value: body.id,
-  //         },
-  //       ],
-  //     },
-  //   ],
-  // };
+  // // prepare call to the Slack API
+  const slackURL = process.env.SLACK_WEBHOOK_URL;
+  const slackPayload = {
+    text: "New comment on " + URL,
+    attachments: [
+      {
+        fallback: "New comment on the comment example site",
+        color: "#444",
+        author_name: "body.data.email",
+        title: "body.data.path",
+        title_link: URL + "body.data.path",
+        text: "body.data.comment",
+      },
+      {
+        fallback: "Manage comments on " + URL,
+        callback_id: "comment-action",
+        actions: [
+          {
+            type: "button",
+            text: "Approve comment",
+            name: "approve",
+            value: "body.id",
+          },
+          {
+            type: "button",
+            style: "danger",
+            text: "Delete comment",
+            name: "delete",
+            value: "body.id",
+          },
+        ],
+      },
+    ],
+  };
 
-  // // // post the notification to Slack
-  // request.post({ url: slackURL, json: slackPayload }, function (
-  //   err,
-  //   httpResponse,
-  //   body
-  // ) {
-  //   var msg;
-  //   if (err) {
-  //     msg = "Post to Slack failed:" + err;
-  //   } else {
-  //     msg = "Post to Slack successful!  Server responded with:" + body;
-  //   }
-  //   callback(null, {
-  //     statusCode: 200,
-  //     body: msg,
-  //   });
-  //   return console.log(msg);
-  // });
+  // // post the notification to Slack
+  request.post({ url: slackURL, json: slackPayload }, function (
+    err,
+    httpResponse,
+    body
+  ) {
+    var msg;
+    if (err) {
+      msg = "Post to Slack failed:" + err;
+    } else {
+      msg = "Post to Slack successful!  Server responded with:" + body;
+    }
+    callback(null, {
+      statusCode: 200,
+      body: msg,
+    });
+    return console.log(msg);
+  });
 };
