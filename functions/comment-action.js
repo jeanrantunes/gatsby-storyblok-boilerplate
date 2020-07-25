25f59db78a70e4cd8aa770dfa0e8390d0a1f43dd
Jean Antunes <jeanrantunes@MacBook-Pro-de-Jean.local>
2020-07-25 15:53:18 -0300

    comment action

--- functions/comment-action.js
+++ functions/comment-action.js
@@ -1,26 +1,31 @@
-var request = require("request");
-
+const request = require("request");
+const axios = require("axios");
 // populate environment variables locally.
 require("dotenv").config();
 const { NETLIFY_AUTH_TOKEN } = process.env;
 
 // hardcoding this for a moment... TODO: replace request with somethign that follows redirects
 const URL = "https://elegant-wiles-a8b2e1.netlify.app";
-
 /*
   delete this submission via the api
 */
 function purgeComment(id) {
-  var url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`;
-  request.delete(url, function (err, response, body) {
-    if (err) {
-      return console.log(err);
-    } else {
-      return console.log("Comment deleted from queue.");
-    }
-  });
-}
+  const url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`;
 
+  axios({
+    method: "delete",
+    url,
+  })
+    .then((resp) => {
+      callback(null, {
+        statusCode: 200,
+        body: "Comment deleted from queue",
+      });
+    })
+    .catch((err) => {
+      callback(null, { statusCode: 500, body: JSON.stringify(error.response) });
+    });
+}
 /*
   Handle the lambda invocation
 */
@@ -31,61 +36,72 @@ exports.handler = async (event, context, callback) => {
   const method = payload.actions[0].name;
   const id = payload.actions[0].value;
 
-  console.log(body);
-  console.log(payload);
-  console.log("method", method);
-  console.log("id", id);
-  // if (method == "delete") {
-  //   purgeComment(id);
-  //   callback(null, {
-  //     statusCode: 200,
-  //     body: "Comment deleted",
-  //   });
-  // } else if (method == "approve") {
-  //   // get the comment data from the queue
-  //   var url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`;
+  if (method === "delete") {
+    purgeComment(id);
+  } else if (method === "approve") {
+    // get the comment data from the queue
+    const url = `https://api.netlify.com/api/v1/submissions/${id}?access_token=${NETLIFY_AUTH_TOKEN}`;
 
-  //   request(url, function (err, response, body) {
-  //     if (!err && response.statusCode === 200) {
-  //       var data = JSON.parse(body).data;
+    axios({
+      method: "get",
+      url,
+    })
+      .then((resp) => {
+        console.log(resp);
+        callback(null, {
+          statusCode: 200,
+          body: resp.statusText,
+        });
+      })
+      .catch((error) => {
+        callback(null, {
+          statusCode: 500,
+          body: JSON.stringify(error.response),
+        });
+      });
 
-  //       // now we have the data, let's massage it and post it to the approved form
-  //       var payload = {
-  //         "form-name": "approved-comments",
-  //         path: data.path,
-  //         received: new Date().toString(),
-  //         email: data.email,
-  //         name: data.name,
-  //         comment: data.comment,
-  //       };
-  //       var approvedURL = URL;
+    // request(url, function (err, response, body) {
+    //   if (!err && response.statusCode === 200) {
+    //     const data = JSON.parse(body).data;
 
-  //       console.log("Posting to", approvedURL);
-  //       console.log(payload);
+    //     // now we have the data, let's massage it and post it to the approved form
+    //     const payload = {
+    //       "form-name": "approved-comments",
+    //       path: data.path,
+    //       received: new Date().toString(),
+    //       email: data.email,
+    //       name: data.name,
+    //       comment: data.comment,
+    //     };
 
-  //       // post the comment to the approved lost
-  //       request.post({ url: approvedURL, formData: payload }, function (
-  //         err,
-  //         httpResponse,
-  //         body
-  //       ) {
-  //         var msg;
-  //         if (err) {
-  //           msg = "Post to approved comments failed:" + err;
-  //           console.log(msg);
-  //         } else {
-  //           msg = "Post to approved comments list successful.";
-  //           console.log(msg);
-  //           purgeComment(id);
-  //         }
-  //         var msg = "Comment registered. Site deploying to include it.";
-  //         callback(null, {
-  //           statusCode: 200,
-  //           body: msg,
-  //         });
-  //         return console.log(msg);
-  //       });
-  //     }
-  //   });
-  // }
+    //     const approvedURL = URL;
+
+    //     console.log("Posting to", approvedURL);
+    //     console.log(payload);
+
+    //     // post the comment to the approved lost
+    //     request.post({ url: approvedURL, formData: payload }, function (
+    //       err,
+    //       httpResponse,
+    //       body
+    //     ) {
+    //       var msg;
+    //       if (err) {
+    //         msg = "Post to approved comments failed:" + err;
+    //         console.log(msg);
+    //       } else {
+    //         msg = "Post to approved comments list successful.";
+    //         console.log(msg);
+    //         purgeComment(id);
+    //       }
+    //       var msg = "Comment registered. Site deploying to include it.";
+    //       callback(null, {
+    //         statusCode: 200,
+    //         body: msg,
+    //       });
+    //       return console.log(msg);
+    //     });
+    //   }
+    // });
+  }
 };
